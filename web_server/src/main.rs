ac8a5c9e544a020f2368ba2b880af54d6dab5bda
Nikhil <nikhofthehil@gmail.com>
2017-05-01 16:41:06 -0500

    Handling a single web request and printing it to stdout

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,3 +1,40 @@
+/**
+ * A web server implemented in Rust
+ */
+
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Binding listener to 127.0.0.1:8080. Press Ctrl+C to quit.");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Error processing TcpStream: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+
+    let mut buffer = String::new();
+
+    match stream.read_to_string(&mut buffer) {
+        Ok(_) => {
+            println!("{}", buffer);
+        },
+        Err(_) => {
+            println!("Could not read TcpStream");
+        },
+
+    }
+
 }
