4bf90e7e84b4a863f3d106db53582003e5338f18
sgp715 <7sebastianperez@gmail.com>
2017-05-03 00:47:44 -0500

    adding tests

--- src/handle_request.rs
+++ src/handle_request.rs
@@ -1,69 +1,72 @@
-pub fn generate_response(request: &str) -> (i64, String) {
-
-
-    (200, "HTTP/1.1 200 OK\n\n<html><body>Hello, World!</body></html>".to_owned())
-
-}
-
-
-fn validate_get_format() -> bool {
+fn validate_get_format(request: &str) -> bool {
     false
 }
 
 #[test]
 fn validate_get_format_test(){
-    assert_eq!(generate_response("GEt /main.rs HTTP"), false);
-    assert_eq!(generate_response("GE /main.rs HTTP"), false);
-    assert_eq!(generate_response("GT /main.rs HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP"), true);
+    assert_eq!(validate_get_format("GEt /main.rs HTTP"), false);
+    assert_eq!(validate_get_format("GE /main.rs HTTP"), false);
+    assert_eq!(validate_get_format("GT /main.rs HTTP"), false);
+    assert_eq!(validate_get_format("GET /main.rs HTTP"), true);
 }
 
-fn validate_file_format() -> bool {
+fn validate_file_format(request: &str) -> bool {
     false
 }
 
 #[test]
-fn validate_get_file_test(){
+fn validate_file_format_test(){
 
-    assert_eq!(generate_response("GET main.rs HTTP"), false);
-    assert_eq!(generate_response("GET /main/ HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs/ HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP"), true);
+    assert_eq!(validate_file_format("GET main.rs HTTP"), false);
+    assert_eq!(validate_file_formatvalidate_file_format("GET /main/ HTTP"), false);
+    assert_eq!(validate_file_format("GET /main.rs/ HTTP"), false);
+    assert_eq!(validate_file_format("GET /main.rs HTTP"), true);
 
 }
 
-fn validate_protocol_format() -> bool {
+fn validate_protocol_format(request: &str) -> bool {
     false
 }
 
 #[test]
 fn validate_protocol_format_test(){
 
-    assert_eq!(generate_response("GET /main.rs HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTT"), false);
-    assert_eq!(generate_response("GET /main.rs HTTp"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP/"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP/1.1"), true);
-    assert_eq!(generate_response("GET /main.rs HTTP/1.0"), true);
-    assert_eq!(generate_response("GET /main.rs HTTP/0.9"), true);
-    assert_eq!(generate_response("GET /main.rs HTTP/0.8"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP"), true);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP"), false);
+    assert_eq!(validate_protocol_format("GET /main.rs HTT"), false);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTp"), false);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP/"), false);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP/1.1"), true);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP/1.0"), true);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP/0.9"), true);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP/0.8"), false);
+    assert_eq!(validate_protocol_format("GET /main.rs HTTP"), true);
 
 }
 
-fn validate_http_format() -> bool {
+fn validate_request_format(request: &str) -> bool {
     false
 }
 
-fn validate_http_test(){
+fn validate_request_format(){
 
-    assert_eq!(generate_response(" GET /main.rs HTTP"), false);
-    assert_eq!(generate_response("GET  /main.rs HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs  HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP "), false);
-    assert_eq!(generate_response(" ueau GET /main.rs HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP aueau"), false);
-    assert_eq!(generate_response("GET /main.rs aueau HTTP"), false);
-    assert_eq!(generate_response("GET /main.rs HTTP"), true);
+    assert_eq!(validate_request_format(" GET /main.rs HTTP"), false);
+    assert_eq!(validate_request_format("GET  /main.rs HTTP"), false);
+    assert_eq!(validate_request_format("GET /main.rs  HTTP"), false);
+    assert_eq!(validate_request_format("GET /main.rs HTTP "), false);
+    assert_eq!(validate_request_format(" ueau GET /main.rs HTTP"), false);
+    assert_eq!(validate_request_format("GET /main.rs HTTP aueau"), false);
+    assert_eq!(validate_request_format("GET /main.rs aueau HTTP"), false);
+    assert_eq!(validate_request_format("GET /main.rs HTTP"), true);
+
+}
+
+
+pub fn generate_response(request: &str) -> (i64, String) {
+
+    if validate_request_format(request) == False {
+        (400, "HTTP/1.1 400 Bad Request\n\n<html><body>You suck...</body></html>".to_owned())
+    }
+
+    (200, "HTTP/1.1 200 OK\n\n<html><body>Hello, World!</body></html>".to_owned())
 
 }
