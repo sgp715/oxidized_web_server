a500e36b65af6d208053f3cc1376f70872cbb31e
Michael Gallagher <michaelgallagher2017@u.northwestern.edu>
2017-05-03 02:12:29 -0500

    request validation

--- src/handle_request.rs
+++ src/handle_request.rs
@@ -1,52 +1,91 @@
 fn validate_get_format(request: &str) -> bool {
-    false
+    if request == "GET"{
+        return true;
+    }
+    return false;
 }
 
 #[test]
 fn validate_get_format_test(){
-    assert_eq!(validate_get_format("GEt /main.rs HTTP"), false);
-    assert_eq!(validate_get_format("GE /main.rs HTTP"), false);
-    assert_eq!(validate_get_format("GT /main.rs HTTP"), false);
-    assert_eq!(validate_get_format("GET /main.rs HTTP"), true);
+    assert_eq!(validate_get_format("GEt"), false);
+    assert_eq!(validate_get_format("GE"), false);
+    assert_eq!(validate_get_format("GT"), false);
+    assert_eq!(validate_get_format("GET"), true);
 }
 
+
 fn validate_file_format(request: &str) -> bool {
-    false
+    let mut chars = request.chars();
+    if chars.next() == Some('/') {
+        if chars.last() == Some('/'){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    return false;
 }
 
 #[test]
 fn validate_file_format_test(){
 
-    assert_eq!(validate_file_format("GET main.rs HTTP"), false);
-    assert_eq!(validate_file_format("GET /main/ HTTP"), false);
-    assert_eq!(validate_file_format("GET /main.rs/ HTTP"), false);
-    assert_eq!(validate_file_format("GET /main.rs HTTP"), true);
+    assert_eq!(validate_file_format("main.rs"), false);
+    assert_eq!(validate_file_format("/main/"), false);
+    assert_eq!(validate_file_format("/main.rs/"), false);
+    assert_eq!(validate_file_format("/main.rs"), true);
 
 }
 
 fn validate_protocol_format(request: &str) -> bool {
-    false
+    let mut chars = request.chars().peekable();
+    if chars.next() ==Some('H')&&chars.next()==Some('T')&&chars.next()==Some('T')&&chars.next()==Some('P'){
+        if chars.peek()==None{
+            return true;
+        }
+        if chars.next() == Some('/'){
+            if chars.peek() ==Some(&'0'){
+                chars.next();
+                if chars.next() ==Some('.')&&chars.next()==Some('9'){
+                    return true;
+                }
+            }
+            else if chars.peek() == Some(&'1'){
+                chars.next();
+                if chars.next() ==Some('.')&&(chars.peek()==Some(&'0')||chars.peek()==Some(&'1')){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 #[test]
 fn validate_protocol_format_test(){
 
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP"), false);
-    assert_eq!(validate_protocol_format("GET /main.rs HTT"), false);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTp"), false);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP/"), false);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP/1.1"), true);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP/1.0"), true);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP/0.9"), true);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP/0.8"), false);
-    assert_eq!(validate_protocol_format("GET /main.rs HTTP"), true);
+    assert_eq!(validate_protocol_format("HTT"), false);
+    assert_eq!(validate_protocol_format("HTTp"), false);
+    assert_eq!(validate_protocol_format("HTTP/"), false);
+    assert_eq!(validate_protocol_format("HTTP/1.1"), true);
+    assert_eq!(validate_protocol_format("HTTP/1.0"), true);
+    assert_eq!(validate_protocol_format("HTTP/0.9"), true);
+    assert_eq!(validate_protocol_format("HTTP/0.8"), false);
+    assert_eq!(validate_protocol_format("HTTP"), true);
 
 }
 
 fn validate_request_format(request: &str) -> bool {
-    false
+    let segments: Vec<&str> = request.split(' ').collect();
+    if segments.len() == 3{
+        if validate_get_format(segments[0])&&validate_file_format(segments[1])&&validate_protocol_format(segments[2]){
+            return true;
+        }
+    }
+    return false;
 }
 
+#[test]
 fn validate_request_format_test(){
 
     assert_eq!(validate_request_format(" GET /main.rs HTTP"), false);
